563fe3becfc3be620044781cf1cc209d5d36975b
Zachary Spector <public@zacharyspector.com>
2025-04-29 09:28:38 +1200

    Upgrade SQLAlchemy recipe to 2.0.40

--- pythonforandroid/recipes/sqlalchemy/__init__.py
+++ pythonforandroid/recipes/sqlalchemy/__init__.py
@@ -3,13 +3,13 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class SQLAlchemyRecipe(CompiledComponentsPythonRecipe):
     name = 'sqlalchemy'
-    version = '1.3.3'
-    url = 'https://pypi.python.org/packages/source/S/SQLAlchemy/SQLAlchemy-{version}.tar.gz'
+    version = '2.0.40'
+    url = 'https://files.pythonhosted.org/packages/68/c3/3f2bfa5e4dcd9938405fe2fab5b6ab94a9248a4f9536ea2fd497da20525f/sqlalchemy-2.0.40.tar.gz'
     call_hostpython_via_targetpython = False
 
-    depends = ['setuptools']
+    depends = ['setuptools', 'typing_extensions']
 
-    patches = ['zipsafe.patch']
+    patches = ['zipsafe.patch', 'typing_extensions.patch']
 
 
 recipe = SQLAlchemyRecipe()
