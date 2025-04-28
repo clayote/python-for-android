1983ff9065e47502fd0d26baaebfa711cd8b29ff
Zachary Spector <public@zacharyspector.com>
2025-04-29 11:07:00 +1200

    Revert "Just remove `typing_extensions.patch`, we don't need it"
    This reverts commit 97f0ff17de2dbff0b34bb0350dd685a94476ee73.

--- pythonforandroid/recipes/sqlalchemy/__init__.py
+++ pythonforandroid/recipes/sqlalchemy/__init__.py
@@ -2,14 +2,14 @@ from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 
 class SQLAlchemyRecipe(CompiledComponentsPythonRecipe):
-	name = "sqlalchemy"
-	version = "2.0.40"
-	url = "https://files.pythonhosted.org/packages/68/c3/3f2bfa5e4dcd9938405fe2fab5b6ab94a9248a4f9536ea2fd497da20525f/sqlalchemy-2.0.40.tar.gz"
-	call_hostpython_via_targetpython = False
+    name = 'sqlalchemy'
+    version = '2.0.40'
+    url = 'https://files.pythonhosted.org/packages/68/c3/3f2bfa5e4dcd9938405fe2fab5b6ab94a9248a4f9536ea2fd497da20525f/sqlalchemy-2.0.40.tar.gz'
+    call_hostpython_via_targetpython = False
 
-	depends = ["setuptools", "typing_extensions"]
+    depends = ['setuptools', 'typing_extensions']
 
-	patches = ["zipsafe.patch"]
+    patches = ['zipsafe.patch', 'typing_extensions.patch']
 
 
 recipe = SQLAlchemyRecipe()
